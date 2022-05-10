2dca0e86b44f16db7a5de08cdbcce268aec65121
Kennytian <kt.tian@gmail.com>
2022-05-10 14:04:04 +0800

    feat: first commit

---
+++ src/main.ts
@@ -0,0 +1,51 @@
+import { ethers } from "ethers";
+
+class EtherStarted {
+  constructor() {}
+
+  signer: any;
+  providers: any;
+  busd: any;
+
+  public init() {
+    const provider = new ethers.providers.Web3Provider({ host: "" });
+  }
+
+  async connect() {
+    this.signer = this.providers.getSigner();
+  }
+
+  async connectContract() {
+    if (!this.signer) {
+      this.init();
+    }
+
+    this.busd = new ethers.Contract(
+      "",
+      ["function balanceOf(address account) external view returns (uint256)"],
+      this.signer
+    );
+  }
+
+  async getBalance() {
+    if (!this.busd) {
+      await this.connectContract();
+    }
+    const balance =   await this.busd.blanceOf("0xd35ac571e4a5AA3d067C87E528415E7ea2612439");
+    console.log("balance is:", balance.toString());
+  }
+
+  async transfer() {
+    if (!this.busd) {
+      await this.connectContract();
+    }
+
+    const tx = await this.busd.transfer("0xd35ac571e4a5AA3d067C87E528415E7ea2612439", "1000000000000000000");
+    const wait = await tx.wait();
+
+    console.log(wait);
+    console.log(wait.transactionHash);
+  }
+}
+
+new EtherStarted().init();
